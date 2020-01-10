e09488a84674e2a881ddc655ea7cae3320cafa2d
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-10 15:38:41 -0800

    add password error handling

--- API/src/routes/authRoutes.js
+++ API/src/routes/authRoutes.js
@@ -32,12 +32,12 @@ router.post('/signin', async (req, res) => {
     }
 
     try {
-    await user.comparePassword(password);
-    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-    res.send({ token });
-    } catch (err) {
-        return res.status(422).send({ error: 'Invalid password or email' });
-    }
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY' );
+        res.send({ token });
+        } catch (err) {
+            return res.status(422).send({ error: 'Invalid password or email' });
+        }
 });
 
 module.exports = router;
