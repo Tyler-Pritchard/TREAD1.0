e1ec22fec1f1046811d9b5dac9874db01c10b700
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-10 14:50:20 -0800

    add jwt auth req middleware

--- API/src/index.js
+++ API/src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -25,8 +26,8 @@ mongoose.connection.on('error', (err) => {
 })
 
 
-app.get('/', (req, res) => {
-    res.send('Hi there!');
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
