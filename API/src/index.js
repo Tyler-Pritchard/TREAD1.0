68300989a1a063a3501fc6adfd1fde55c363b189
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-10 00:11:00 -0800

    add mongoose

--- API/src/index.js
+++ API/src/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://root:toor@cluster0-ygila.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
 
 app.get('/', (req, res) => {
     res.send('Hi there!');
