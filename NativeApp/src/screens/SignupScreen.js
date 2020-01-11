d866553977d6fcd6ad1064ac3be2cf653d8a5ef9
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-10 20:44:22 -0800

    center signup content

--- NativeApp/src/screens/SignupScreen.js
+++ NativeApp/src/screens/SignupScreen.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
     return (
-        <>
-            <Text h3>Sign Up for TREAD</Text>
-            <Input label="Email" />
-            <Input label="Password" />
-            <Button title="Sign Up" />
-        </>
+        <View style={styles.container}>
+            <Spacer>
+                <Text h3>Sign Up for TREAD</Text>
+            </Spacer>
+                <Input label="Email" />
+            <Spacer />
+                <Input label="Password" />
+            <Spacer>
+                <Button title="Sign Up" />
+            </Spacer>
+        </View>
     );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    header: null
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250
+    }
+});
 
 export default SignupScreen;
