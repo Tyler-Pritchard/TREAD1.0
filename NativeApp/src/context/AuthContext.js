127c9d552da8dc24c202ec1fd72c54b99c01ed29
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-11 15:39:20 -0800

    signup error messaging rendering

--- NativeApp/src/context/AuthContext.js
+++ NativeApp/src/context/AuthContext.js
@@ -1,20 +1,23 @@
+import { AsyncStorage } from 'react-native';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload }
         default:
             return state;
     }
 };
 
-const signup = (dispatch) => {
+const signup = dispatch => {
     return async ({ email, password }) => {
         try {
             const response = await trackerApi.post('/signup', { email, password });
             console.log(response.data);
         } catch (err) {
-            console.log(err.message);
+            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
         }
     };
 };
@@ -34,5 +37,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signin, signout, signup },
-    { isSignedIn: false }
+    { isSignedIn: false, errorMessage: '' }
 );
