e17c3a30fcf688ef32f001c124dd3efebc93c5fd
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-10 20:29:41 -0800

    create signup input fields

--- NativeApp/src/screens/SignupScreen.js
+++ NativeApp/src/screens/SignupScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
 
 const SignupScreen = ({ navigation }) => {
     return (
         <>
-            <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-            <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')} />
-            <Button title="Go to main flow" onPress={() => navigation.navigate('mainFlow')} />
+            <Text h3>Sign Up for TREAD</Text>
+            <Input label="Email" />
+            <Input label="Password" />
+            <Button title="Sign Up" />
         </>
     );
 };
