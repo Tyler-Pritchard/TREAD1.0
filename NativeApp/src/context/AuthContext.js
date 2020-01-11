82fd1510fe76b1565e41734f2d3ecb4ad981267e
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-11 15:45:33 -0800

    add token to authReducer, export provider

--- NativeApp/src/context/AuthContext.js
+++ NativeApp/src/context/AuthContext.js
@@ -6,6 +6,8 @@ const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
             return { ...state, errorMessage: action.payload }
+        case 'signup':
+            return { errorMessage: '', token: action.payload };
         default:
             return state;
     }
@@ -15,7 +17,8 @@ const signup = dispatch => {
     return async ({ email, password }) => {
         try {
             const response = await trackerApi.post('/signup', { email, password });
-            console.log(response.data);
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({ type: 'signup', payload: response.data.token });
         } catch (err) {
             dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
         }
@@ -37,5 +40,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signin, signout, signup },
-    { isSignedIn: false, errorMessage: '' }
+    { token: null, errorMessage: '' }
 );
