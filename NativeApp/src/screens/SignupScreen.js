5a1cfcc29435e96ff778ce94c669bf5630b5b60a
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-11 12:07:35 -0800

    add state/useState to signup screen

--- NativeApp/src/screens/SignupScreen.js
+++ NativeApp/src/screens/SignupScreen.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <View style={styles.container}>
             <Spacer>
                 <Text h3>Sign Up for TREAD</Text>
             </Spacer>
-                <Input label="Email" />
+                <Input 
+                    label="Email" 
+                    value={email} 
+                    onChangeText={setEmail}
+                />
             <Spacer />
                 <Input label="Password" />
             <Spacer>
