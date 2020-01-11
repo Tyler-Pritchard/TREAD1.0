18641057600d8989931e3f81057981b7df4a6bfb
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-10 18:38:33 -0800

    rebuild native project folder

--- NativeApp/App.js
+++ NativeApp/App.js
@@ -1,16 +1,19 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import HomeScreen from './src/screens/HomeScreen';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  {
-    initialRouteName: 'Home',
-    defaultNavigationOptions: {
-      title: 'App'
-    }
-  }
-);
-
-export default createAppContainer(navigator);
+});
