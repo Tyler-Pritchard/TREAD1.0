daf1b99d63d17d768a6048e881d154c0ecfcd697
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-11 16:57:02 -0800

    create NavLink template

---
+++ NativeApp/src/components/NavLink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Spacer from './Spacer';
+import { withNavigation } from 'react-navigation';
+
+const NavLink = () => {
+
+};
+
+const styles = StyleSheet.create({});
+
+export default NavLink;
