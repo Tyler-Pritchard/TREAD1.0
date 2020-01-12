afbd7407558ab2ed46073b010ccc3e182f48b118
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-12 12:32:23 -0800

    update ngrok baseURL

--- NativeApp/src/api/tracker.js
+++ NativeApp/src/api/tracker.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     //ngrok url provided by running 'ngrok http <portNumberOfAPIInstance>' from separate terminal window
 
-    baseURL: 'http://bb324840.ngrok.io'
+    baseURL: 'http://0ba966cb.ngrok.io'
 
 })
