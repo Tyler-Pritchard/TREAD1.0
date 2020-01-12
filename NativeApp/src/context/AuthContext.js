e83ef7da735760eb178d3d8ac06cddf81705cb48
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-11 16:07:27 -0800

    navigate to tracklist screen on signup

--- NativeApp/src/context/AuthContext.js
+++ NativeApp/src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import { AsyncStorage } from 'react-native';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
+import { navigate } from '../navigationRef';
 
 const authReducer = (state, action) => {
     switch (action.type) {
@@ -13,17 +14,21 @@ const authReducer = (state, action) => {
     }
 };
 
-const signup = dispatch => {
-    return async ({ email, password }) => {
-        try {
-            const response = await trackerApi.post('/signup', { email, password });
-            await AsyncStorage.setItem('token', response.data.token);
-            dispatch({ type: 'signup', payload: response.data.token });
-        } catch (err) {
-            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
-        }
-    };
+const signup = dispatch => async ({ email, password }) => {
+    try {
+        const response = await trackerApi.post('/signup', { email, password });
+        await AsyncStorage.setItem('token', response.data.token);
+        dispatch({ type: 'signup', payload: response.data.token });
+
+        navigate('TrackList');
+    } catch (err) {
+        dispatch({ 
+            type: 'add_error', 
+            payload: 'Something went wrong with sign up' 
+        });
+    }
 };
+
 
 const signin = (dispatch) => {
     return ({ email, password }) => {
