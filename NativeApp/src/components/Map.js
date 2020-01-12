7fa0a5e6ad3845a9c8ee7d02b7700d32badf99b9
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-12 12:43:40 -0800

    import MapView component, map rendering

--- NativeApp/src/components/Map.js
+++ NativeApp/src/components/Map.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 const Map = () => {
-    return <Text>I am a map.  I am an Island.</Text>;
+    return <MapView style={styles.map} />
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        height: 300
+    }
+});
 
 export default Map;
