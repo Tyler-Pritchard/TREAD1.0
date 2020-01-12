c40d3c109e98ea91d7d9f7e9fb6fc9d67a364cde
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-12 12:48:53 -0800

    ...

--- NativeApp/src/components/Map.js
+++ NativeApp/src/components/Map.js
@@ -3,7 +3,15 @@ import { Text, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 
 const Map = () => {
-    return <MapView style={styles.map} />
+    return <MapView 
+        style={styles.map} 
+        initialRegion={{
+            latitude:37.33233,
+            longitude: -122.03121,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01
+        }}
+    />
 };
 
 const styles = StyleSheet.create({
