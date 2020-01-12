3b5123d7a784ee67b64a0fd949a674178f84a7e8
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-11 17:10:48 -0800

    build out sign in screen with component

--- NativeApp/src/screens/SigninScreen.js
+++ NativeApp/src/screens/SigninScreen.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink'
 
 const SigninScreen = () => {
-    return <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+    return (
+        <View style={styles.container}>
+            <AuthForm 
+                headerText="Sign In to Your Account"
+                errorMessage=""
+                onSubmit={() => {}}
+                submitButtonText="Sign In"
+            />
+            <NavLink 
+                text="Don't have an account? Sign up instead." 
+                routeName="Signup"
+
+            />
+        </View>
+    )
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.apply.navigationOptions = {
+    header: null
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250
+    }
+
+});
 
 export default SigninScreen;
