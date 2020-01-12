7a869cb018ed45a95d7072f2128f8d2c88f50b3a
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-12 12:40:08 -0800

    create maps template

---
+++ NativeApp/src/components/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const Map = () => {
+    return <Text>I am a map.  I am an Island.</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default Map;
