ceb0d7dabb85c8269bfac4b8b30e67e8ac51aa9b
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-11 16:28:44 -0800

    create AuthForm template

---
+++ NativeApp/src/components/AuthForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Button, Input } from 'react-native-elements';
+
+const AuthForm = () => {
+    
+};
+
+const styles = StyleSheet.create({});
+
+export default AuthForm;
--- NativeApp/src/screens/SignupScreen.js
+++ NativeApp/src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
@@ -36,6 +36,13 @@ const SignupScreen = ({ navigation }) => {
             <Spacer>
                 <Button title="Sign Up" onPress={() => signup({ email, password })} />
             </Spacer>
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Spacer>
+                    <Text style={styles.link}>
+                        Already have an account? Sign in instead
+                    </Text>
+                </Spacer>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -53,6 +60,9 @@ const styles = StyleSheet.create({
     errorMessage: {
         fontSize: 16,
         color: 'red'
+    },
+    link: {
+        color: 'blue'
     }
 });
 
