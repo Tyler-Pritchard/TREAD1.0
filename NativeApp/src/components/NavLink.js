d9ec8731026ebfa07a6e8bc211c9012c915bf85c
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-11 17:02:41 -0800

    signin link extracted to separate component

--- NativeApp/src/components/NavLink.js
+++ NativeApp/src/components/NavLink.js
@@ -3,10 +3,22 @@ import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Spacer from './Spacer';
 import { withNavigation } from 'react-navigation';
 
-const NavLink = () => {
-
+const NavLink = ({ navigation, text, routeName }) => {
+    return (
+    <TouchableOpacity onPress={() => navigation.navigate(routeName)}>
+        <Spacer>
+            <Text style={styles.link}>
+                {text}
+            </Text>
+        </Spacer>
+    </TouchableOpacity>
+    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({ 
+    link: {
+        color: 'blue'
+    }
+});
 
-export default NavLink;
+export default withNavigation(NavLink);
