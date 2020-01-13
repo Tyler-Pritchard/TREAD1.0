cf2b84238e48680a1df09ee0dbc37a5a75e47e83
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-13 01:56:59 -0800

    import tracks as flatlist

--- NativeApp/src/screens/TrackListScreen.js
+++ NativeApp/src/screens/TrackListScreen.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { ListItem } from 'react-native-elements';
+import { Context as TrackContext } from '../context/TrackContext';
 
 const TrackListScreen = ({ navigation }) => {
-    return <> 
-    <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-    <Button
-        title="Go to Track Detail"
-        onPress={() => navigation.navigate('TrackDetail')} />
-    </>
+    const { state, fetchTracks } = useContext(TrackContext);
+
+    return (
+        <> 
+            <NavigationEvents onWillFocus={fetchTracks} />
+            <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+            <FlatList
+                data={state}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return ( 
+                        <TouchableOpacity>
+                            <ListItem chevron title={item.name} />
+                        </TouchableOpacity>
+                    );
+                }}
+            />
+        </>
+    );
 };
 
 const styles = StyleSheet.create({});
