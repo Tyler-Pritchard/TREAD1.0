10f665ba2a292405f03656fffd670db3ffa48d54
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-12 23:11:30 -0800

    update useEffect in useLocation with callback

--- NativeApp/src/hooks/useLocation.js
+++ NativeApp/src/hooks/useLocation.js
@@ -33,7 +33,13 @@ export default (shouldTrack, callback) => {
             subscriber.remove();
             setSubscriber(null);
         }
-    }, [shouldTrack]);
+
+        return () => {
+            if (subscriber) {
+                subscriber.remove();
+            }
+        };
+    }, [shouldTrack, callback]);
 
     return [err];
 };
