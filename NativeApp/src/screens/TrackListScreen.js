e4995a3676f14d27262225e83d6ed34cd8ef4475
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-13 02:52:45 -0800

    replace text in navbar w/ icons

--- NativeApp/src/screens/TrackListScreen.js
+++ NativeApp/src/screens/TrackListScreen.js
@@ -10,7 +10,6 @@ const TrackListScreen = ({ navigation }) => {
     return (
         <> 
             <NavigationEvents onWillFocus={fetchTracks} />
-            <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
             <FlatList
                 data={state}
                 keyExtractor={item => item._id}
@@ -29,6 +28,10 @@ const TrackListScreen = ({ navigation }) => {
         </>
     );
 };
+
+TrackListScreen.navigationOptions = {
+    title: 'Tracks'
+}
 
 const styles = StyleSheet.create({});
 
