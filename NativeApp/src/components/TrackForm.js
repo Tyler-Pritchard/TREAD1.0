0d51d17cdadd2c7392abd0b57f8c765a78439d10
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-12 16:23:28 -0800

    create trackForm component

---
+++ NativeApp/src/components/TrackForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Input, Button } from 'react-native-elements';
+
+const TrackForm = () => {
+    return <>
+        <Input />
+        <Button title="Start Recording" />
+    </>
+};
+
+export default TrackForm;
