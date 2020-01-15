9e4a552d8d8287e057d53faede72e93c38e26704
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-14 22:26:17 -0800

    remove typo, comment out mock location import from TrackCreateScreen

--- NativeApp/src/components/Map.js
+++ NativeApp/src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { StyleSheet, ActivityIndicator } from 'react-native';
 import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
