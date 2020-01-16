f9ee9f0f0249656955c6fbd6c84e155fa42c0f07
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-16 13:24:38 -0800

    ngrok update

--- NativeApp/src/api/tracker.js
+++ NativeApp/src/api/tracker.js
@@ -3,7 +3,7 @@ import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
     //ngrok url provided by running 'ngrok http <portNumberOfAPIInstance>' from separate terminal window
-    baseURL: 'http://8f6c2b46.ngrok.io'
+    baseURL: 'http://5b7d1f0a.ngrok.io'
 });
 
 instance.interceptors.request.use(
