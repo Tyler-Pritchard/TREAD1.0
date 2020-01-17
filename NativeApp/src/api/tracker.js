723679f7b152f7a20fbcce67f0ee8f55c879d457
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-01-16 18:48:30 -0800

    set baseURL to EC2 instance

--- NativeApp/src/api/tracker.js
+++ NativeApp/src/api/tracker.js
@@ -3,7 +3,10 @@ import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
     //ngrok url provided by running 'ngrok http <portNumberOfAPIInstance>' from separate terminal window
-    baseURL: 'http://5b7d1f0a.ngrok.io'
+    // baseURL: 'http://5b7d1f0a.ngrok.io'
+
+    //AWS istance
+    baseURL: 'ec2-18-222-134-134.us-east-2.compute.amazonaws.com'
 });
 
 instance.interceptors.request.use(
