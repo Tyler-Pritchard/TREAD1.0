d7dd95682676d784521f277694fb5b988c3576ce
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-02-09 02:40:00 -0800

    add basic styling to tracks and account screens

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -3,37 +3,37 @@ import { StyleSheet, Text, FlatList, TouchableOpacity, View } from 'react-native
 import { NavigationEvents } from 'react-navigation';
 import { ListItem } from 'react-native-elements';
 import { Context as TrackContext } from '../context/TrackContext';
-import { LinearGradient } from 'expo-linear-gradient';
+import { Buttons, Colors, Spacing } from '../styles'
 
 const TrackListScreen = ({ navigation }) => {
     const { state, fetchTracks } = useContext(TrackContext);
-
+    
     return (
         <> 
             <View style={styles.container}>
-                <LinearGradient
-                    colors={["#08AEEA", "#2AF598", "#08AEEA"]}
-                    style={{ padding: 15, borderRadius: 5 }}>
                     <NavigationEvents onWillFocus={fetchTracks} />
-                    <FlatList style={styles.list}
+                    <FlatList style={styles.buttonsContainer}
                         data={state}
                         keyExtractor={item => item._id}
                         renderItem={({ item }) => {
+                            
                             return ( 
                                 <TouchableOpacity 
                                     onPress={() => 
                                         navigation.navigate('TrackDetail', { _id: item._id })
                                     }
+                                    
                                 >
+                                  
                                     <ListItem 
                                     chevron 
-                                    title={item.name}      
+                                    title={item.name}
                                     />
+                               
                                 </TouchableOpacity>    
                             );
                         }}
                 />
-                </LinearGradient>
             </View>
         </>
     );
@@ -45,16 +45,23 @@ TrackListScreen.navigationOptions = {
 
 const styles = StyleSheet.create({
     container: {
-        borderRadius: 4,
-        borderWidth: 0.5,
-        borderColor: 'blue',
-        flex: 1,
         justifyContent: 'center',
-        marginBottom: 250
+        marginBottom: 10,
+        backgroundColor: Colors.background,
+        borderRadius: 5,
+        flex: 1,
+        marginHorizontal: Spacing.sectionPadding,
+        paddingHorizontal: Spacing.small,
+        paddingVertical: Spacing.base,
     },
-    list: {
-        backgroundColor: "blue"
-    }
+    buttonsContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        flex: 1,
+      },
+      buttonText: {
+        ...Buttons.text,
+      },
 });
 
 export default TrackListScreen;
