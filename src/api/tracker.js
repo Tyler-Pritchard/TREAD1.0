a6668ec5c99b0a5a5dfe78ef955761cad3063000
Your Name <you@example.com>
2020-10-31 21:51:55 -0700

    functioning w/ updated json, still getting intermittent react version mismatch error

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -3,10 +3,10 @@ import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
     //ngrok url provided by running 'ngrok http <portNumberOfAPIInstance>' from separate terminal window
-     //baseURL: 'http://069284a3.ngrok.io'
+     baseURL: 'http://8cddbdb9ee86.ngrok.io'
 
     //AWS istance
-    baseURL: 'https://tread-dev-api.herokuapp.com/'
+    //baseURL: 'https://tread-dev-api.herokuapp.com/'
 });
 
 instance.interceptors.request.use(
