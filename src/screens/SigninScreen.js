0c9d9003b632021a4d2e0f90371ec5d450afd8dc
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-11-05 18:19:15 -0800

    update package.json

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -4,10 +4,27 @@ import { NavigationEvents } from 'react-navigation';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 import { Context } from '../context/AuthContext';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { Button } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
 
 const SigninScreen = () => {
     const { state, signin, clearErrorMessage } = useContext(Context);
-
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        expoClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+        iosClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+        androidClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+        webClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+      });
+    
+      React.useEffect(() => {
+        if (response.type === 'success') {
+          const { authentication } = response;
+          }
+      }, [response]);
+    
     return (
         <View style={styles.container}>
             <NavigationEvents 
@@ -24,6 +41,13 @@ const SigninScreen = () => {
                 routeName="Signup"
 
             />
+            <Button
+          disabled={!request}
+          title="Login"
+          onPress={() => {
+            promptAsync();
+            }}
+        />
         </View>
     )
 };
