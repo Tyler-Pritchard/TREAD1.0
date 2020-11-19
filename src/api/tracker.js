b700351ff86357f270c5ec179883638af3d92d4a
Your Name <you@example.com>
2020-11-18 22:01:59 -0800

    update

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -3,11 +3,7 @@ import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
     //ngrok url provided by running 'ngrok http <portNumberOfAPIInstance>' from separate terminal window
-<<<<<<< HEAD
-     baseURL: 'http://de28928bd40c.ngrok.io'
-=======
-     baseURL: 'http://2caeae7c0c3b.ngrok.io'
->>>>>>> 573bbcf8dbef37311a9c1e13e7845d278a63e4c5
+     baseURL: 'http://b2d6566fd244.ngrok.io';
 
     //AWS istance
     //baseURL: 'https://tread-dev-api.herokuapp.com/'
