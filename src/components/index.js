074dc7f32a7d1b785b300094472940e9a277e94c
Tyler Pritchard <pritchard.tyler@gmail.com>
2021-06-09 20:19:21 -0700

    relocate the smh readme into legacy

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ import Button from './Button';
 import Select from './Select';
 import Icon from './Icon';
 import Tabs from './Tabs';
-import Product from './Product';
 import Drawer from './Drawer';
 import Header from './Header';
 import Switch from './Switch';
@@ -12,7 +11,6 @@ export {
   Select,
   Icon,
   Tabs,
-  Product,
   Drawer,
   Header,
   Switch,
